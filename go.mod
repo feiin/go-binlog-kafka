c8418d239b5696b9e6ef2b5d31e0171f52e120c5
Yin Yang <feiin@qq.com>
2023-05-28 15:52:12 +0800

    feat: parse binlog event

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/feiin/go-binlog-kafka
+
+go 1.18
+
+require github.com/go-mysql-org/go-mysql v1.7.0
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/pingcap/errors v0.11.5-0.20210425183316-da1aaba5fb63 // indirect
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
+	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
+	github.com/siddontang/go-log v0.0.0-20180807004314-8d05993dda07 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/go-mysql-org/go-mysql/mysql"
+	"github.com/go-mysql-org/go-mysql/replication"
+)
+
+func main() {
+	// dbInstanceName := flag.String("db_instance_name", "", "Database instance name")
+	// kafkaTopicName := flag.String("kafka_topic_name", "", "Kafka topic name")
+	// kafkaAddrr := flag.String("kafka_addr", "", "Kafka address")
+	// adminHost := flag.String("admin_host", "", "admin manage db host")
+	// adminPort := flag.String("admin_port", "3306", "admin manage db port")
+	// adminUser := flag.String("admin_user", "", "admin manage db user")
+	// adminPass := flag.String("admin_pass", "", "admin manage db password")
+	srcDbUser := flag.String("src_db_user", "", "sync source db user")
+	srcDbPass := flag.String("src_db_pass", "", "sync source db password")
+	srcDbHost := flag.String("src_db_host", "", "sync source db host")
+	srcDbPort := flag.Int("src_db_port", 3306, "sync source db port")
+	replicationId := flag.Int("replication_id", 212388888, "replication id")
+	binlogTimeout := flag.Int64("binlog_timeout", 0, "binlog max read timeout")
+	flag.Parse()
+
+	cfg := replication.BinlogSyncerConfig{
+		ServerID: uint32(*replicationId),
+		Flavor:   "mysql",
+		Host:     *srcDbHost,
+		Port:     uint16(*srcDbPort),
+		User:     *srcDbUser,
+		Password: *srcDbPass,
+	}
+
+	syncer := replication.NewBinlogSyncer(cfg)
+	gtid, _ := mysql.ParseGTIDSet("mysql", "68414ab6-fd2a-11ed-9e2d-0242ac110002:1")
+	streamer, _ := syncer.StartSyncGTID(gtid)
+
+	for {
+
+		var ev *replication.BinlogEvent
+		var err error
+		if *binlogTimeout > 0 {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*binlogTimeout)*time.Millisecond)
+			ev, err = streamer.GetEvent(ctx)
+			cancel()
+			if err == context.DeadlineExceeded {
+				fmt.Printf("GetEventTimeout timeout")
+				continue
+			}
+			if err != nil {
+				fmt.Printf("GetEvent error:%v", err)
+				break
+			}
+
+		} else {
+			ev, err = streamer.GetEvent(context.Background())
+			if err != nil {
+				fmt.Printf("GetEvent error:%v", err)
+				break
+			}
+		}
+
+		event := ev.Header.EventType
+
+		switch event {
+		case replication.WRITE_ROWS_EVENTv2, replication.WRITE_ROWS_EVENTv1:
+			rowsEvent := ev.Event.(*replication.RowsEvent)
+			for _, row := range rowsEvent.Rows {
+				fmt.Printf("insert row %v\n", row)
+			}
+		case replication.UPDATE_ROWS_EVENTv2, replication.UPDATE_ROWS_EVENTv1:
+			rowsEvent := ev.Event.(*replication.RowsEvent)
+			for _, row := range rowsEvent.Rows {
+				fmt.Printf("update row %v\n", row)
+			}
+
+		case replication.DELETE_ROWS_EVENTv2, replication.DELETE_ROWS_EVENTv1:
+			rowsEvent := ev.Event.(*replication.RowsEvent)
+			for _, row := range rowsEvent.Rows {
+				fmt.Printf("delete row %v\n", row)
+			}
+
+		case replication.QUERY_EVENT:
+			queryEvent := ev.Event.(*replication.QueryEvent)
+			fmt.Printf("query event %v\n", queryEvent)
+		case replication.GTID_EVENT:
+			gtidEvent := ev.Event.(*replication.GTIDEvent)
+			fmt.Printf("gtid event %v\n", gtidEvent)
+
+		case replication.TABLE_MAP_EVENT:
+			tableMapEvent := ev.Event.(*replication.TableMapEvent)
+			fmt.Printf("table map event %v\n", tableMapEvent)
+
+		case replication.ROTATE_EVENT:
+			rotateEvent := ev.Event.(*replication.RotateEvent)
+			fmt.Printf("rotate event %v\n", rotateEvent)
+		default:
+			fmt.Printf("no action with event %v\n", event)
+		}
+	}
+
+}
